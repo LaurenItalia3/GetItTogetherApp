ba0a34054546b4d5736241f1423c207820881576
Diya Patel <dipate69@gmail.com>
2022-07-19 12:53:56 -0400

    Initial Commit

---
+++ GetItTogetherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetItTogetherApp
+//
+//  Created by Diya Patel on 7/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
